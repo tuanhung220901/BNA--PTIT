0d14c8e5a98a7007dd0a5dccce5dfaa0a4881a98
tuanhung220901 <hoangtuanhung220901@gmail.com>
2022-03-11 18:29:15 +0700

    update scrren moblie

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("lop-phu").style.display = "block";
+    console.log("hung");
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("lop-phu").style.display = "none";
+  }
